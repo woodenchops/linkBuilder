c535288bc1b271a27408f9a6ca50f3b2fe5fe67a
kristian woods <kris_woods@hotmail.co.uk>
2019-11-06 17:09:32 +0000

    build query string in url

--- main.js
+++ main.js
@@ -1,25 +1,122 @@
-function DeepLinkBuilder(){
+function DeepLinkModel(){
 
-    var protocolInput = document.getElementById('protocol'),
-        environmentInput = document.getElementById('environment'),
-        localeInput = document.getElementById('locale'),
-        brandInput = document.getElementById('brand'),
-        searchBookInput = document.getElementById('searchBook'),
-        ctyhocnInput = document.getElementById('ctyhocn'),
-        ctyhocnCheckBox = document.getElementById('ctyhocn-checkbox'),
-        result = document.getElementById('result'),
-        copyButton = document.getElementById('copyText'),
-        self = this;
+    // params obj
+
+    this._params = {};
 
     // check if field is empty 
 
     this.checkIfEmpty = function(field) {
-        if(field === undefined || field === null ) {
-            return "";
-        } else {
-            return field;
-        }
+       return (field) ? field : '';
     }
+
+    // protocol section
+
+    this.setProtocol = function(val) {
+        this._protocolValue = val;
+    }
+
+    this.getProtocol = function() {
+        return this.checkIfEmpty(this._protocolValue);
+    }
+
+    // env section
+
+    this.setEnv = function(val) {
+        this._env = val;
+    }
+
+    this.getEnv = function() {
+       return this.checkIfEmpty(this._env);
+    }
+
+    // locale section
+
+    this.setLocale = function(val) {
+        this._locale = val;
+    }
+
+    this.getLocale = function() {
+        return this.checkIfEmpty(this._locale);
+    }
+
+    // brand section
+
+    this.setBrand = function(val) {
+        this._brandValue = val;
+    }
+
+    this.getBrand = function() {
+        return this.checkIfEmpty(this._brandValue);
+    }
+
+    // Search/Book section
+
+    this.setSearchBook = function(val) {
+        this._searchBook = val;
+    }
+
+    this.getSearchBook = function() {
+        return this.checkIfEmpty(this._searchBook);
+    }
+
+    this.setParam = function(param,val){
+        this._params[param] = val;
+    }
+
+    this.getParam = function(param){
+        return this._params[param];
+    }
+
+    this.removeParam = function(param){
+        delete this._params[param];
+    }
+
+    this.buildParamString = function(){
+
+        var queryString = "";
+
+        if(Object.entries(this._params).length > 0){
+
+            Object.entries(this._params).forEach( (val,idx) => {
+                queryString += (idx === 0)? '?' : '&'; // build the url by starting off by adding "?" at start then "&" inbetween params
+                queryString += val[0] + '=' + val[1]; // params key/value pairs 
+            });
+        }
+
+        return queryString;
+
+    }
+
+    // get the URL
+
+    this.getURL = function() {
+        return this.getProtocol() + 
+               this.getEnv() + 
+               this.getLocale() +  
+               this.getBrand() + 
+               this.getSearchBook() +
+               this.buildParamString();
+    }
+
+}
+
+function DeeplinkUI() {
+
+    var protocolInput = document.getElementById('protocol'),
+    environmentInput = document.getElementById('environment'),
+    localeInput = document.getElementById('locale'),
+    brandInput = document.getElementById('brand'),
+    searchBookInput = document.getElementById('searchBook'),
+    ctyhocnInput = document.getElementById('ctyhocn'),
+    ctyhocnCheckBox = document.getElementById('ctyhocn-checkbox'),
+    specPlanInput = document.getElementById('spec_plan'),
+    specPlanCheckBox = document.getElementById('spec_plan-checkbox'),
+    result = document.getElementById('result'),
+    copyButton = document.getElementById('copyText'),
+    self = this;
+
+    this.deeplinkModel = new DeepLinkModel();
 
     // add button to allow user to copy link
 
@@ -31,129 +128,69 @@ function DeepLinkBuilder(){
 
     copyButton.addEventListener('click', this.copyLink);
 
-
-    // protocol section
-
-    this.setProtocol = function(val) {
-        this._protocolValue = val;
-        this.generateURL();
-    }
-
-    this.getProtocol = function() {
-        return this.checkIfEmpty(this._protocolValue);
-    }
-
-    // env section
-
-    this.setEnv = function(val) {
-        this._env = val;
-        this.generateURL();
-    }
-
-    this.getEnv = function() {
-       return this.checkIfEmpty(this._env);
-    }
-
-
-    // locale section
-
-    this.setLocale = function(val) {
-        this._locale = val;
-        this.generateURL();
-    }
-
-    this.getLocale = function() {
-        return this.checkIfEmpty(this._locale);
-    }
-
-
-    // brand section
-
-    this.setBrand = function(val) {
-        this._brandValue = val;
-        this.generateURL();
-    }
-
-    this.getBrand = function() {
-        return this.checkIfEmpty(this._brandValue);
-    }
-
-
-    // Search/Book section
-
-    this.setSearchBook = function(val) {
-        this._searchBook = val;
-        this.generateURL();
-    }
-
-    this.getSearchBook = function() {
-        return this.checkIfEmpty(this._searchBook);
-    }
-
-    // ctyhocn section
-
-    this.setCtyhocn = function(val) {
-
-        if(val.length <= 0) {
-            ctyhocnCheckBox.checked = false;
-            this._ctyhocn = "";
-            this.generateURL();
-        } else {
-            var param = ctyhocnInput.getAttribute('data-param');
-            this._ctyhocn = param + '=' + val;
-            this.generateURL();
-        }
-    }
-
-    this.getCtyhocn = function() {
-        return this.checkIfEmpty(this._ctyhocn);
-    }
-
-    // create the URL
-
-    this.generateURL = function() {
-        return result.value = this.getURL()
-    }
-    // get the URL
-
-    this.getURL = function() {
-        return this.getProtocol() + 
-               this.getEnv() + 
-               this.getLocale() +  
-               this.getBrand() + 
-               this.getSearchBook() +
-               this.getCtyhocn();
+    this.generateURL = function () {
+        result.value = this.deeplinkModel.getURL();
     }
 
     // all the event listeners for each input field
 
     protocolInput.addEventListener('input', function(e) {
-        this.setProtocol(e.target.value);
+        this.deeplinkModel.setProtocol(e.target.value);
+        this.generateURL();
     }.bind(self));
 
     environmentInput.addEventListener('input', function(e) {
-        this.setEnv(e.target.value);
+        this.deeplinkModel.setEnv(e.target.value);
+        this.generateURL();
     }.bind(self));
 
     localeInput.addEventListener('input', function(e) {
-        this.setLocale(e.target.value);
+        this.deeplinkModel.setLocale(e.target.value);
+        this.generateURL();
     }.bind(self));
 
     brandInput.addEventListener('input', function(e) {
-        this.setBrand(e.target.value);
+        this.deeplinkModel.setBrand(e.target.value);
+        this.generateURL();
     }.bind(self));
 
     searchBookInput.addEventListener('input', function(e) {
-        this.setSearchBook(e.target.value);
+        this.deeplinkModel.setSearchBook(e.target.value);
+        this.generateURL();
     }.bind(self));
 
+    // ctyhocn
 
-    ctyhocnInput.addEventListener('input', function(e) {
-        this.setCtyhocn(e.target.value);
+    ctyhocnCheckBox.addEventListener('input', function(e) {
+        if(!ctyhocnCheckBox.checked) {
+            ctyhocnInput.value = '';
+            this.deeplinkModel.removeParam('ctyhocn');
+            this.generateURL();
+        }
     }.bind(self));
-
     
+    ctyhocnInput.addEventListener('input', function(e) {
+        var param = ctyhocnInput.getAttribute('data-param');
+        this.deeplinkModel.setParam(param,e.target.value);
+        this.generateURL();
+    }.bind(self));
+
+    // spec plan
+
+    specPlanCheckBox.addEventListener('input', function(e) {
+        if(!specPlanCheckBox.checked) {
+            specPlanInput.value = '';
+            this.deeplinkModel.removeParam('spec_plan');
+            this.generateURL();
+        }
+    }.bind(self));
+
+    specPlanInput.addEventListener('input', function(e) {
+        var param = specPlanInput.getAttribute('data-param');
+        this.deeplinkModel.setParam(param,e.target.value);
+        this.generateURL();
+    }.bind(self));
 
 }
 
-var deeplink = new DeepLinkBuilder();
+var formUI = new DeeplinkUI();
